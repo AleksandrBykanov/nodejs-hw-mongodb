3aa0e136d391cdbe01c36c2a420e44eff0af0644
Aleksandr Bykanov <aleksandrbykanov1986@gmail.com>
2024-10-16 10:59:20 +0300

    make connect

--- src/index.js
+++ src/index.js
@@ -1,3 +1,9 @@
+import { initMongoConnection } from "./db/initMongoConnection.js";
 import { setupServer } from "./server.js";
 
-setupServer()
+const start = async () => {
+    await initMongoConnection();
+    setupServer();
+};
+
+start();
--- src/server.js
+++ src/server.js
@@ -1,9 +1,10 @@
 import express from 'express';
-
+import cors from 'cors';
 
 export function setupServer() {
   const app = express();
   const PORT = process.env.PORT || 3000;
+  app.use(cors());
 
   app.get('/', (req, res) => {
     res.json({
